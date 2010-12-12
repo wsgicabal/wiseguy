f4523e94b28961725e390c88d4aabe3819127229
Dave Brondsema <dave@brondsema.net>
2010-12-12 15:08:49 -0500

    minor fixes for new paste adapter

--- wiseguy/adapter/paste.py
+++ wiseguy/adapter/paste.py
@@ -1,7 +1,10 @@
+from __future__ import absolute_import
+
 import colander
 from wiseguy import component
 
 import paste.gzipper
+
 class GZipSchema(component.WiseSchema):
     compress_level = colander.SchemaNode(
         colander.Int(),
