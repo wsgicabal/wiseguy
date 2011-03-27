6b5fd9927b4515bfdf0faa03edbc5d46ccfa82b5
Chris McDonough <chrism@plope.com>
2011-03-27 10:41:14 -0400

    move wsgiapp tests to schema tests

--- wiseguy/tests/test_schema.py
+++ wiseguy/tests/test_schema.py
@@ -65,3 +65,44 @@ class TestUrlType(unittest.TestCase):
         url = self._makeOne()
         from colander import Invalid
         self.assertRaises(Invalid, url.serialize, None, 1)
+
+class Test_WSGIApp(unittest.TestCase):
+    def _makeOne(self, loader):
+        return schema._WSGIApp(loader)
+
+    def test_deserialize_bad_cstruct(self):
+        from colander import Invalid
+        loader = DummyLoader()
+        inst = self._makeOne(loader)
+        self.assertRaises(Invalid, inst.deserialize, None, None)
+
+    def test_deserialize_loader_raises(self):
+        from colander import Invalid
+        loader = DummyRaisingLoader()
+        inst = self._makeOne(loader)
+        self.assertRaises(Invalid, inst.deserialize, None, 'abc')
+
+    def test_deserialize_ok(self):
+        loader = DummyLoader()
+        inst = self._makeOne(loader)
+        result = inst.deserialize(None, 'abc')
+        self.assertEqual(result, 'abc')
+
+
+class TestWSGIApp(unittest.TestCase):
+    def _makeOne(self):
+        return schema.WSGIApp(None, {'loader':DummyLoader()})
+
+    def test_it(self):
+        from wiseguy.schema import _WSGIApp
+        inst = self._makeOne()
+        self.assertEqual(inst.__class__, _WSGIApp)
+
+class DummyLoader(object):
+    def get_app_factory(self, cstruct):
+        return cstruct
+
+class DummyRaisingLoader(object):
+    def get_app_factory(self, cstruct):
+        raise RuntimeError
+
